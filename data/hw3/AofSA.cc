88e88fc445a6de02bb249da6ff0166879d32d97f
ahow6552 <91654201+ahow6552@users.noreply.github.com>
2022-12-25 22:27:39 +0800

    rm BS func

--- data/hw3/AofSA.cc
+++ data/hw3/AofSA.cc
@@ -48,31 +48,6 @@ bool array_of_sorted_arrays::search(int target) const{
 	return false;
 }
 
-/*int binary_search(int arr[], int l, int r, int x)
-{
-    if (r >= l) {
-        int mid = l + (r - l) / 2;
- 
-        // If the element is present at the middle
-        // itself
-        if (arr[mid] == x)
-            return mid;
- 
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
- 
-        // Else the element can only be present
-        // in right subarray
-        return binarySearch(arr, mid + 1, r, x);
-    }
- 
-    // We reach here when element is not
-    // present in array
-    return -1;
-}*/
-
 vector<int> array_of_sorted_arrays::merge(const vector<int>& v1, const vector<int>&v2) const{
 	vector<int> v;
 	int x = 0, y = 0;
